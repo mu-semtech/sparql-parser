9e5f47756283408458545f7c7ec12467a05a3420
Aad Versteden <madnificent@gmail.com>
2024-05-16 11:16:14 +0200

    Wake delta message bus immediately on messages
    Introduces a semaphore which will wake the message bus when a new
    message is put on the queue.  An optional timeout can be set (currently
    set to 60 seconds) to indicate how long we want to wait when there are
    no messages.  This is mostly to circumvent undiscovered race conditions
    and it could be set to nil too.

--- delta/message-bus.lisp
+++ delta/message-bus.lisp
@@ -1,25 +1,40 @@
 (in-package :delta-messenger)
 
-(defparameter *message-bus-queue* nil)
-(defparameter *message-bus-lock* (bt:make-lock "messenger"))
-(defparameter *sleep-on-idle-bus* 1)
+(defparameter *message-bus-queue* nil
+  "Current queue on the message bus containing unconsumed messages.")
+(defparameter *message-bus-lock* (bt:make-lock "messenger-lock")
+  "Lock ensuring we don't modify the bus at the same time.")
+(defparameter *message-bus-semaphore* (bt:make-semaphore :name "messenger-sem")
+  "Semaphore used to notify about changes on the bus.
+   Should be modified only with *MESSAGE-BUS-LOCK* but should not break.")
+(defparameter *max-sleep-on-idle-bus* 60
+  "Amount of seconds to sleep when the bus is considered idle.
+
+   This is a safety setting that would cover an erroneous semaphore
+   implementation. It could be set to NIL to disable the feature.")
 (defparameter *message-bus-consumer* nil
   "Consumer to be called for each message on the message bus.")
+(defparameter *log-delta-messenger-message-bus-processing* t
+  "Logs when the delta messenger runs and for what.")
 
 (defun message-bus-loop ()
   "Loop for the delta message bus."
   (handler-case
       (let (available-messages)
+        (bt:wait-on-semaphore *message-bus-semaphore* :timeout *max-sleep-on-idle-bus*)
         (bt:with-lock-held (*message-bus-lock*)
           (setf available-messages (nreverse (copy-list *message-bus-queue*))) ; reverse takes a copy
           (setf *message-bus-queue* nil))
-        (dolist (message available-messages)
-          (apply *message-bus-consumer* message))
-        (unless available-messages
-          ;; TODO: make thread sleep if nothing is on the bus and wake through SCHEDULE-DELTA-MESSAGE
-          (sleep *sleep-on-idle-bus*)))
+        (if available-messages
+            (progn
+              (when *log-delta-messenger-message-bus-processing*
+                (format t "~&[INFO][DELTA-BUS] Delta messenger will process ~A messages~%"
+                        (length available-messages)))
+              (dolist (message available-messages)
+                (apply *message-bus-consumer* message)))
+            (when *log-delta-messenger-message-bus-processing*
+              (format t "~&[INFO][DELTA-BUS] Delta messenger woke without available message.~%"))))
     (error (e) (format t "~&Error occurred on messenger message bus!~% ~A~%; waiting 3s and continuing next cycle, ignoring current messages~&" e)))
-  ;; (format t "~&Processing ~A delta messages from bus" (length available-messages))
   (message-bus-loop))
 
 (defun launch-message-consumer (consuming-function)
@@ -31,4 +46,10 @@
   "schedules messages for the notifier system."
   (bt:with-lock-held (*message-bus-lock*)
     (setf *message-bus-queue*
-          (nconc (reverse messages) *message-bus-queue*))))
+          (nconc (reverse messages) *message-bus-queue*))
+    (unless (> (sb-thread:semaphore-count *message-bus-semaphore*) 0)
+      ;; the consuming thread might already be awake and not have the
+      ;; lock which means we'd still wake the consuming thread twice
+      ;; even without delta messages being available.  That is not a big
+      ;; problem, it will just be an extra empty loop on that end.
+      (sb-thread:signal-semaphore *message-bus-semaphore*))))
